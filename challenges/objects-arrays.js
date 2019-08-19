37295f7f576c6f9dc06dbbec322375c5b5fa3cbe
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-08-18 23:15:18 -0400

    objects-array.js is all done, no errors, once again! yay!

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -72,15 +72,9 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-function Universities() {
-let newArray = [];
-for (let i = 0; i < graduates.length; i ++) {
-  universities.push(graduates[i].university);
-  }
-  return newArray.sort();
-};
-console.log(universities);
+const universities = graduates.map((info) => info.university); 
+const sortedUniversities = universities.sort();
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
