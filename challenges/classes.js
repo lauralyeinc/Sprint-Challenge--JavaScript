51c311b28d326aeba726746d6d0b6a584b0bea4f
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-08-18 23:09:05 -0400

    Was working on stretch goal in classes.js. Was double checking to make the formual for a cube was the same as a cuboid. And then, building my code and used different numbers to make sure it wasn't just overwritting.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,7 @@
 
 class CuboidMakerclass {
   constructor(detail) {
-    this.length = detai.length,
+    this.length = detail.length,
     this.width = detail.width,
     this.height = detail.height
   }
@@ -20,3 +20,26 @@ console.log(CuboidObject.volume()); // 100
 console.log(CuboidObject.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMakerclass {
+  constructor(detail) {
+    super(detail);
+  }
+  volume = () => {
+    let volumeEquals = this.length * this.width * this.height;
+    return volumeEquals;
+  }
+  surfaceArea = () => {
+    let CuboidSurfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    return CuboidSurfaceArea;
+  }
+}
+
+const CubeObject = new CubeMaker ({
+  length: 8,
+  width: 10,
+  height: 10,
+});
+
+console.log(CubeObject.volume());
+console.log(CubeObject.surfaceArea());
